f202fb95432d9d320ee0fc3faba789145328f63d
helloworldzxy <zxy_bupt@sina.com>
2017-06-16 21:43:37 +0800

    cannot login successfully and don't know why

---
+++ index.js
@@ -0,0 +1,14 @@
+var net = require('net');
+
+/**
+ * 初始化IRC客户端
+ */
+var client = net.connect(6667, 'irc.freenode.net');
+
+client.setEncoding('utf-8');
+
+client.on('connect', function(){
+	client.write('NICK mynick\r\n');
+	client.write('USER mynick 0 * : realname\r\n');
+	client.write('JOIN #node.js\r\n');
+});
